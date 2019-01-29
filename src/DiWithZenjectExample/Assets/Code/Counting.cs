b8798b68a99444d89707ed5efff647b651f8dd5c
Daniel Schroeder <deadlydog@hotmail.com>
2019-01-28 20:33:16 -0600

    Added button and text UI controls, presenter, and model

---
+++ src/DiWithZenjectExample/Assets/Code/Counting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Assets.Code
+{
+	public class Counter
+	{
+		private int _currentCount;
+
+		public event Action<int> Incremented;
+
+		public void Increment()
+		{
+			_currentCount++;
+			Incremented?.Invoke(_currentCount);
+		}
+	}
+}
